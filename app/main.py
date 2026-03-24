a32097eb776b81d79a0f11debb00ece70be14a91
Camopants <gdr+cb@courtfields.com>
2026-03-24 00:33:01 +0000

    playing with lights

--- app/main.py
+++ app/main.py
@@ -5,11 +5,16 @@
 
 import app
 import imu
-import math
+
+from math import sqrt, atan, atan2
 
 from app_components import clear_background
+#from app_components.tokens import line_height
 from events.input import Buttons, BUTTON_TYPES
-from app_components.tokens import line_height
+
+from system.eventbus import eventbus
+from system.patterndisplay.events import PatternEnable, PatternDisable
+from tildagonos import tildagonos
 
 THRESHOLD=2
 
@@ -18,6 +23,7 @@ class BarAssistApp(app.App):
         self.button_states = Buttons(self)
         self.orientation = None
         self.rotation_offset = 0
+        self.led_control = False
 
     def __get_orientation(self):
 
@@ -25,25 +31,30 @@ class BarAssistApp(app.App):
         self.ox = float('{0:.1f}'.format(acc_read[0] * 9))
         self.oy = float('{0:.1f}'.format(acc_read[1] * 9))
         self.oz = float('{0:.1f}'.format(acc_read[2] * 9))
+        self.theta = 0 if self.ox == 0 else atan(self.oy/self.ox)
 
         orientation = 0 if self.orientation==None else self.orientation
         # vaguely upright - no action
         if self.ox>75:
             orientation = 0
-        # umop apisdn
-        elif self.ox<0:
-            orientation = 3
         # lying down or tilted too far
-        #elif abs(self.oz)>45 or self.ox>abs(self.oy):
         elif abs(self.oz)>45:
             orientation = 1
-        elif abs(self.oy)>abs(self.ox):
+        else:
+            weighting = min(1.0, int(abs(10 - acc_read[2])) / 9)
+            self.rotation_offset = (atan2(acc_read[1], acc_read[0])) * weighting
+
+            # umop apisdn
+            if self.ox<0:
+                orientation = 3
+            #elif abs(self.oz)>45 or self.ox>abs(self.oy):
+            elif abs(self.oy)>abs(self.ox):
             # sideways left
-            if self.oy<0:
-                orientation = 2
-            # sideways right
-            else:
-                orientation = 4
+                if self.oy<0:
+                    orientation = 2
+                # sideways right
+                else:
+                    orientation = 4
         return orientation
 
     #def update(self, delta):
@@ -52,9 +63,24 @@ class BarAssistApp(app.App):
             # The button_states do not update while you are in the background.
             # Calling clear() ensures the next time you open the app, it stays
             # open. Without it the app would close again immediately.
+            print('clear buttons')
             self.button_states.clear()
+            print('re-enable pattern')
+            if self.led_control:
+                eventbus.emit(PatternEnable())
+                self.led_control = False
+            print('minimise')
             self.minimise()
-            print('exit')
+            return
+        else:
+            if not self.led_control:
+                print('disable pattern')
+                eventbus.emit(PatternDisable())
+                self.led_control = True
+
+# messages:
+# No assistance currently required
+# Please return to the upright position
 
     def draw(self, ctx):
         def place_text(self, msg, l=0, r=0, g=0.5, b=0.25):
@@ -64,52 +90,60 @@ class BarAssistApp(app.App):
         newo = self.__get_orientation()
         if self.orientation != newo:
             self.orientation = newo
-            v = math.sqrt(self.ox*self.ox+self.oy*self.oy+self.oz*self.oz)
-            print(f'({self.ox:.1f}, {self.oy:.1f}, {self.oz:.1f})')
-            print(f'Orientation: {newo}')
-            print(f'Vector: {v}')
+            #v = sqrt(self.ox*self.ox+self.oy*self.oy+self.oz*self.oz)
+            m1 = f'({self.ox:.1f}, {self.oy:.1f}, {self.oz:.1f})'
+            m2 = f'Orientation: {newo}'
+            m3 = f'Theta: {self.theta:.2f}'
 
-            ctx.save()
+            #ctx.save()
             #ctx.font = ctx.get_font_name(5)
             ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
-            t = f'({self.ox}, {self.oy}, {self.oz})'
-            place_text(self, t)
-            t= f'Orientation: {newo}'
-            place_text(self, t, l=1)
-            t= f'Vector: {v:.1f}'
-            place_text(self, t, l=2)
-            ctx.restore()
+            place_text(self, m1, l=2)
+            place_text(self, m2, l=3)
+            place_text(self, m3, l=4)
+            #ctx.restore()
+            print(m1)
+            print(m2)
+            print(m3)
 
+            self.set_leds()
+
+    def set_leds(self):
+        if not self.led_control:
+            print('disable pattern')
+            eventbus.emit(PatternDisable())
+            self.led_control = True
+
+        null_colour = (0, 0, 15)
+
+        for index in range(12):
+            if self.orientation==0:
+                led_colour = (0, 63, 0)
+            else:
+                led_colour = (63, 0, 0)
+            if self.orientation==1:
+                # turn everything on (flat on back)
+                for i in range(1, 13):
+                    tildagonos.leds[i] = led_colour
+            else:
+                # turn everything on ("down" indication)
+                for i in range(1, 13):
+                    tildagonos.leds[i] = null_colour
+                if self.orientation==2:
+                    tildagonos.leds[9] = led_colour
+                    tildagonos.leds[10] = led_colour
+                elif self.orientation==3:
+                    tildagonos.leds[12] = led_colour
+                    tildagonos.leds[1] = led_colour
+                elif self.orientation==4:
+                    tildagonos.leds[3] = led_colour
+                    tildagonos.leds[4] = led_colour
+                else:
+                    tildagonos.leds[6] = led_colour
+                    tildagonos.leds[7] = led_colour
+                #pass
+
+        tildagonos.leds.write()
 
 __app_export__ = BarAssistApp
 
-##- def olabel():
-##-     #ugfx.set_default_font(#ugfx.FONT_MEDIUM)
-##-     ##ugfx.text(5, 160, str("{:3d}".format(orientation))+" ("+str("{:0.2f}".format(ox))+","+str("{:0.2f}".format(oy))+")", ugfx.WHITE)
-##-     #ugfx.text(5, #ugfx.height() - 20, str("{:3d}".format(orientation))+" ("+str("{:0.2f}".format(ox))+","+str("{:0.2f}".format(oy))+","+str("{:0.2f}".format(oz))+")", ugfx.WHITE)
-##- 
-##- def screen_l0():
-##-     #ugfx.clear(#ugfx.html_color(0x7c1143))
-##-     #ugfx.set_default_font(#ugfx.FONT_NAME)
-##-     #ugfx.text(5,  10, "No assistance ", ugfx.WHITE)
-##-     #ugfx.text(5,  60, "currently ", ugfx.WHITE)
-##-     #ugfx.text(5, 110, "required ", ugfx.WHITE)
-##-     olabel()
-##- 
-##- def screen_l1():
-##-     #ugfx.clear(#ugfx.html_color(0x7c1143))
-##-     #ugfx.set_default_font(#ugfx.FONT_NAME)
-##-     #ugfx.text(5,  10, "Please return ", ugfx.WHITE)
-##-     #ugfx.text(5,  60, "to upright ", ugfx.WHITE)
-##-     #ugfx.text(5, 110, "position ", ugfx.WHITE)
-##-     olabel()
-##- 
-##- def screen_p():
-##-     #ugfx.clear(#ugfx.html_color(0x7c1143))
-##-     #ugfx.set_default_font(#ugfx.FONT_NAME)
-##-     #ugfx.text(5,  10, "Please ", ugfx.WHITE)
-##-     #ugfx.text(5,  60, "return to ", ugfx.WHITE)
-##-     #ugfx.text(5, 110, "upright ", ugfx.WHITE)
-##-     #ugfx.text(5, 160, "position ", ugfx.WHITE)
-##-     olabel()
-##- 
